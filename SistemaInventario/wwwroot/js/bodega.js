b979fbf94da23bd69d8d8fa81d3bdd5ba8c380ef
fabianrojas <fabianrojasrozo@gmail.com>
2023-11-03 19:00:39 -0500

    CRUD de bodegas

---
+++ SistemaInventario/wwwroot/js/bodega.js
@@ -0,0 +1,71 @@
+﻿let datatable
+
+$(document).ready(function () {
+    loadDatatable();
+});
+
+
+function loadDatatable() {
+    datatable = $('#tblDatos').DataTable({
+        "ajax": {
+            "url": "/Admin/Bodega/ObtenerTodos"
+        },
+        "columns": [
+            { "data": "nombre", "width": "20%" },
+            { "data": "descripcion", "width": "40%" },
+            {
+                "data": "estado",
+                "render": function (data) {
+                    if (data == true) {
+                        return "Activo";
+                    }
+                    else
+                    {
+                        return "Inactivo";
+                    }
+                }, "width": "20%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <div class="text-center">
+                    <a href="/Admin/Bodega/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                    <i class="bi bi-pencil-square"></i>                    
+                    </a>
+                    <a  onclick=Delete("/Admin/Bodega/Delete/${data}") class="btn btn-danger text-black" style="cursor:pointer" )>
+                    <i class="bi bi-trash3-fill"></i>
+                    </a>
+                    </div>
+                    `
+                }, "width":"20%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Esta seguro de eliminar la bodega?",
+        Text: "Este registro no se podra recuperar",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((borrar) => {
+        if (borrar) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        datatable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
